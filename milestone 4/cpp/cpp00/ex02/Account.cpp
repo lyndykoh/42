3f9220a1507579f1cf59edb4ce07bde2eaa7ee9c
lyndykoh <lyndykoh03@gmail.com>
2025-05-14 18:21:41 +0800

    cpp00 ex02

--- milestone 4/cpp/cpp00/ex02/Account.cpp
+++ milestone 4/cpp/cpp00/ex02/Account.cpp
@@ -6,9 +6,60 @@
 /*   By: lkoh <lkoh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 20:27:02 by lkoh              #+#    #+#             */
-/*   Updated: 2025/05/08 20:27:31 by lkoh             ###   ########.fr       */
+/*   Updated: 2025/05/14 16:48:48 by lkoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
+#include <ctime>
 
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account() {}
+
+void Account::_displayTimestamp() {
+    time_t timestamp;
+    char output[50];
+    struct tm * datetime;
+    time(&timestamp);
+    datetime = localtime(&timestamp);
+
+    strftime(output, 50, "[%Y%m%d_%H%M%S]", datetime);
+    std::cout << output << " ";
+}
+
+int Account::getNbAccounts() {
+    return _nbAccounts;
+}
+
+int Account::getTotalAmount() {
+    return _totalAmount;
+}
+
+int Account::getNbDeposits() {
+    return _totalNbDeposits;
+}
+
+int Account::getNbDeposits() {
+    return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals() {
+    return _totalNbWithdrawals;
+}
+
+void Account::displayAccountsInfos() {
+    _displayTimestamp();
+    std::cout << "accounts:" << _nbAccounts << ";" 
+              << "total:" << _totalAmount << ";"
+              << "deposits:" << _totalNbDeposits << ";"
+              << "withdrawals:" << _totalNbWithdrawals << std::endl;
+}
+
+void Account::makeDeposit(int deposit) {
+    
+}
