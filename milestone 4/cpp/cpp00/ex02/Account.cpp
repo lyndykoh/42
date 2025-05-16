d61e956ad96f99d7f5f6b3cfdb0f9f53d813ab29
lyndykoh <lyndykoh03@gmail.com>
2025-05-16 14:49:24 +0800

    cpp00 ex02

--- milestone 4/cpp/cpp00/ex02/Account.cpp
+++ milestone 4/cpp/cpp00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: lkoh <lkoh@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 20:27:02 by lkoh              #+#    #+#             */
-/*   Updated: 2025/05/14 16:48:48 by lkoh             ###   ########.fr       */
+/*   Updated: 2025/05/16 14:48:59 by lkoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,59 @@ void Account::displayAccountsInfos() {
 }
 
 void Account::makeDeposit(int deposit) {
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";" 
+              << "p_amount:" << _totalAmount << ";"
+              << "deposit:" << deposit << ";";
+
+    _totalAmount += deposit;
+    _amount += deposit;
+    _totalNbDeposits++;
+    _nbDeposits++;
+
+    std::cout << "amount:" << _amount << ";"
+              << "nb_deposits:" << _nbDeposits << std::endl;
+}
+
+bool Account::makeWithdrawal(int withdrawal) {
+    _displayTimestamp();
     
+    if (_amount >= withdrawal) {
+    std::cout << "index:" << _accountIndex << ";"
+              << "p_amount:" << _totalAmount << ";"
+              << "withdrawal:" << withdrawal << ";";
+              
+    _totalAmount += withdrawal;
+    _amount += withdrawal;
+    _totalNbWithdrawals++;
+    _nbWithdrawals++;  
+    
+    std::cout << "amount:" << _amount << ";"
+              << "nb_withdrawals:" << _nbWithdrawals << std::endl;
+    return (true);
+    }
+    else {
+        std::cout << "index:" << _accountIndex << ";"
+                  << "p_amount:" << _totalAmount << ";"
+                  << "withdrawal: refused" << ";";
+        return (false);
+    }
+}
+
+int Account::checkAmount() const {
+    return (_amount);
+}
+
+void Account::displayStatus() const {
+    std::cout << "index:" << _accountIndex << ";"
+              << "p_amount:" << _totalAmount << ";"
+              << "deposits:" << _nbDeposits << ";"
+              << "withdrawals:" << _nbWithdrawals;
+}
+
+Account::~Account() {
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";"
+              << "amount:" << _amount << ";"
+              << "closed";
 }
